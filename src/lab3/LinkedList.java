e02560f9501a25c373808347e46b895c61e0ed12
achter4n <achter4n@uregina.ca>
2017-10-20 02:04:40 -0600

    added comments throughout the program

--- src/lab3/LinkedList.java
+++ src/lab3/LinkedList.java
@@ -1,11 +1,12 @@
 package lab3;
 
 public class LinkedList {
-	private ListElement front;
-	private ListElement current;
-	private ListElement end;
-	private int numElements;
+	private ListElement front;			//front node of the list
+	private ListElement current;		//current node for traversing the list
+	private ListElement end;			//end node of the list
+	private int numElements;			//number of elements in the list
 	
+	//constructor
 	public LinkedList() {
 		this.front = null;
 		this.current = null;
@@ -13,23 +14,31 @@ public class LinkedList {
 		this.numElements = 0;
 	}
 	
+	//add a new element to the end of the list
 	public void addElement(ListElement le) {
+		//if the front equals null, the list if empty, insert element to the front
 		if(front == null) {
 			front = end = le;
 			return;
 		}
+		//otherwise, add the element to the end of the list
 		end.setNext(le);
 		le.setPrevious(end);
 		end = le;
-		numElements++;
+		numElements++;					//increment number of elements
 	}
 	
+	//retrieve an element from an index
 	public ListElement getElement(int index) {
+		//if the index is negative, the element does not exist
 		if (index < 0)
 			return null;
+		//if the list is not empty
 		if (front != null) {
 			current = front;
+			//for the length of index, traverse the list until reaching the element at the index
 			for(int i = 0; i < index; i++) {
+				//if there is no next element, the element does not exist
 				if(current.getNext() == null)
 					return null;
 				current = current.getNext();
@@ -38,35 +47,43 @@ public class LinkedList {
 		return current;
 	}
 	
+	//delete an element from the list from an index
 	public ListElement deleteElement(int index) {
-		ListElement temp1 = new ListElement();
-		ListElement temp2 = new ListElement();
-		current = getElement(index);
+		ListElement temp1 = new ListElement();	//temp element for previous
+		ListElement temp2 = new ListElement();	//temp element for next
+		current = getElement(index);			//find the element at index
+		//if current was not set to an element, it does not exist
 		if (current == null) {
 			System.out.println("Element does not exist.");
 			return null;
 		}
+		//if current was set as equal to front element, set previous node to null
 		if (current == front) {
 			front = current.getNext();
 			front.setPrevious(null);
 		}
+		//else, if current was set as equal to end element, set nect node to null
 		else if (current == end) {
 			end = current.getPrevious();
 			end.setNext(null);
 		}
+		//else, the element is in the middle of the list, change the pointers to pull it out of the list
 		else {
 			temp1 = current.getPrevious();
 			temp2 = current.getNext();
 			temp1.setNext(temp2);
 			temp2.setPrevious(temp1);
 		}
-		numElements--;
+		numElements--;							//decrement number of elements in the list
 		return current;
 	}
 	
+	//print the list from the tail first
 	public void printLinkedListTail() {
+		//ensure list is not empty
 		if (end != null) {
 			current = end;
+			//while current equals an element, traverse from the rear and print list
 			while (current != null) {
 				System.out.println(current.getData());
 				current = current.getPrevious();
@@ -74,9 +91,12 @@ public class LinkedList {
 		}
 	}
 	
+	//print the list from the head first
 	public void printLinkedListHead() {
+		//ensure list is not empty
 		if (front != null) {
 			current = front;
+			//while current equals an element, traverse from the front and print list
 			while (current != null) {
 				System.out.println(current.getData());
 				current = current.getNext();
--- src/lab3/LinkedListTest.java
+++ src/lab3/LinkedListTest.java
@@ -4,34 +4,35 @@ import java.util.Scanner;
 
 public class LinkedListTest {
 	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		int userInput;
-		int data;
-		int index;
-		Scanner in = new Scanner(System.in);
+		LinkedList list = new LinkedList();		//linked list
+		int userInput;							//user input for switch statement
+		int data;								//data input
+		int index;								//index input
+		Scanner in = new Scanner(System.in);	//console input scanner
 		
 		System.out.println("Enter a '1' to add a node, a '2' to delete a node, a '3' to print the list from the head, a '4' to print the list from the tail, or any other key to end the program.");
 		userInput = in.nextInt();
 		
+		//while the userInput variable equals one of the options
 		while(userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4) {
 			switch (userInput) {
-			case 1:
+			case 1:								//add new element
 				System.out.println("Enter the data to add: ");
 				data = in.nextInt();
 				ListElement le = new ListElement();
 				le.setData(data);
 				list.addElement(le);
 				break;
-			case 2:
+			case 2:								//delete element by index
 				System.out.println("Enter the index of the data to delete: ");
 				index = in.nextInt();
 				list.deleteElement(index);
 				break;
-			case 3:
+			case 3:								//print list from head
 				list.printLinkedListHead();
 				break;
 			case 4:
-				list.printLinkedListTail();
+				list.printLinkedListTail();		//print list from the tail
 				break;
 			}
 			
