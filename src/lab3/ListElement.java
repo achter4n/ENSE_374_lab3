757949e8486e19c5cb03106c62644401ae2e6494
achter4n <achter4n@uregina.ca>
2017-10-19 23:08:55 -0600

    Filled ListElement class

--- src/lab3/ListElement.java
+++ src/lab3/ListElement.java
@@ -1,5 +1,26 @@
-package lab3;
-
 public class ListElement {
-
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement() {
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	/**
+	 * @param data
+	 * 	an integer to be stored in the node
+	 * @return
+	 * 	None
+	 **/
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
 }
