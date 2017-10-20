5eeb485577368595705093aed531668a0c8dc10f
achter4n <achter4n@uregina.ca>
2017-10-19 23:13:29 -0600

    Created private variables and constructor and method names

--- src/lab3/LinkedList.java
+++ src/lab3/LinkedList.java
@@ -1,3 +1,33 @@
 public class LinkedList {
+	private ListElement front;
+	private ListElement current;
+	private ListElement end;
+	private int numElements;
+	
+	public LinkedList() {
+		this.front = null;
+		this.current = null;
+		this.end = null;
+		this.numElements = 0;
+	}
+	
+	public void addElement(ListElement le) {
 
+	}
+	
+	public ListElement getElement(int index) {
+		
+	}
+	
+	public ListElement deleteElement(int index) {
+		
+	}
+	
+	public void printLinkedListTail() {
+		
+	}
+	
+	public void printLinkedListHead() {
+		
+	}
 }
