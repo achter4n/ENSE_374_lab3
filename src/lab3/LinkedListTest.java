1a9c31914ef89a9de8c3e925cedb1f187bb31986
achter4n <achter4n@uregina.ca>
2017-10-20 01:45:32 -0600

    Editted the main method for doubly linked list implementation

--- src/lab3/LinkedListTest.java
+++ src/lab3/LinkedListTest.java
@@ -10,10 +10,10 @@ public class LinkedListTest {
 		int index;
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Enter a '1' to add a node, a '2' to delete a node, a '3' to print the list, or any other key to end the program.");
+		System.out.println("Enter a '1' to add a node, a '2' to delete a node, a '3' to print the list from the head, a '4' to print the list from the tail, or any other key to end the program.");
 		userInput = in.nextInt();
 		
-		while(userInput == 1 || userInput == 2 || userInput == 3) {
+		while(userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4) {
 			switch (userInput) {
 			case 1:
 				System.out.println("Enter the data to add: ");
@@ -30,9 +30,12 @@ public class LinkedListTest {
 			case 3:
 				list.printLinkedListHead();
 				break;
+			case 4:
+				list.printLinkedListTail();
+				break;
 			}
 			
-			System.out.println("Enter a '1' to add a node, a '2' to delete a node, a '3' to print the list, or any other key to end the program.");
+			System.out.println("Enter a '1' to add a node, a '2' to delete a node, a '3' to print the listfrom the head, a '4' to print the list from the tail, or any other key to end the program.");
 			userInput = in.nextInt();
 		}
 	}
